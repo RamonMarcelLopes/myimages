f4da7deb3e6aaa3ce9ee19d56c90abf124f46ecf
jacare-camabox <ramonlindo2311@gmail.com>
2022-11-21 23:18:45 -0300

    feat: website

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-// const fs = require('fs');
-
 let pegadiv = document.querySelector('#imgList');
 
-// let teste = () => {
-//   const dir = './imgs/icons';
-//   const names = [];
-
-//   fs.readdir(dir, (err, arquivos) => {
-//     arquivos.forEach((arquivo) => {
-//       names.push(arquivo);
-//       console.log(names, '✔');
-//     });
-//   });
-// };
-
-let carregar = () => {
-  let imagem = ['https://google.com/google.jpg', './imgs/icons/correct.png'];
+function carregar() {
+  let imagem = [
+    './imgs/icons/add-friend.png',
+    './imgs/icons/correct.png',
+    './imgs/icons/addfriend-black.png',
+    './imgs/icons/arrow-down.png',
+    './imgs/icons/brain.png',
+    './imgs/icons/config.png',
+    './imgs/icons/delete.png',
+    './imgs/icons/editar.png',
+    './imgs/icons/graphic-black.png',
+    './imgs/icons/graphic-white.png',
+    './imgs/icons/group.png',
+    './imgs/icons/lixo.png',
+    './imgs/icons/pencil.png',
+    './imgs/icons/search-black.png',
+    './imgs/icons/search-white.png',
+    './imgs/icons/users.png',
+    './imgs/logos/logoequipe4.png',
+    './imgs/miscellaneous/coisaboa.png',
+    './imgs/miscellaneous/discordjs.png',
+    './imgs/miscellaneous/fantauva.png',
+    './imgs/miscellaneous/petLove.png',
+    './imgs/miscellaneous/qualofluxo.png',
+    './imgs/miscellaneous/refrigerantes.png',
+  ];
 
   imagem.forEach((imagem) => {
     pegadiv.insertAdjacentHTML(
@@ -28,5 +38,6 @@ let carregar = () => {
     `
     );
   });
-};
-// carregar();
+}
+carregar();
+//import brain from './imgs/icons/brain.png';
